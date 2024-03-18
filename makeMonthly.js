99adfc4cda18adcbeee8c28a2a2dfdc3511ac6d6
Taisuke Fukuno <fukuno@jig.jp>
2024-03-18 09:12:30 +0900

    less memory usage

--- makeMonthly.js
+++ makeMonthly.js
@@ -1,50 +1,24 @@
 import { CSV } from "https://js.sabae.cc/CSV.js";
 import { dir2array } from "https://js.sabae.cc/dir2array.js";
-//import { Day } from "https://js.sabae.cc/DateTime.js";
 
 const list = (await dir2array("./daily")).filter(f => f.length == "20220527.csv".length && f.endsWith(".csv")).sort();
-const mlist = {};
-const ylist = {};
-const all = [];
+const mlist = [];
+let mname = null;
+const write = async () => {
+  if (mname) {
+    await Deno.writeTextFile("monthly/" + mname, CSV.stringify(mlist));
+    console.log(mname);
+  }
+};
 for (const fn of list) {
-  const csv = CSV.toJSON(await CSV.fetch("daily/" + fn));
+  const csv = await CSV.fetchJSON("daily/" + fn);
   // monthly
   const fn2 = fn.substring(0, 6) + ".csv";
-  const ml = mlist[fn2];
-  console.log(fn, fn2, fn2 == "202305", ml ? ml.length : 0);
-  if (ml) {
-    csv.forEach(c => ml.push(c));
-  } else {
-    const ml2 = mlist[fn2] = [];
-    csv.forEach(c => ml2.push(c));
+  if (fn2 != mname) {
+    write();
+    mlist.length = 0;
   }
-  
-  /*
-  // fiscal yearly
-  const day = fn.substring(0, 8);
-  const d = new Day(day);
-  const fn3 = "" + d.getFiscalYear() + ".csv";
-  const yl = ylist[fn3];
-  if (yl) {
-    csv.forEach(c => yl.push(c));
-  } else {
-    const yl2 = ylist[fn3] = [];
-    csv.forEach(c => yl2.push(c));
-  }
-  // all
-  csv.forEach(c => all.push(c));
-  */
+  mname = fn2;
+  csv.forEach(c => mlist.push(c));
 }
-for (const name in mlist) {
-  const data = mlist[name];
-  await Deno.writeTextFile("monthly/" + name, CSV.stringify(data));
-  console.log(name);
-}
-/*
-for (const name in ylist) {
-  const data = ylist[name];
-  await Deno.writeTextFile("fiscalyearly/" + name, CSV.stringify(data));
-  console.log(name);
-}
-await Deno.writeTextFile("all.csv", CSV.stringify(all));
-*/
+write();
