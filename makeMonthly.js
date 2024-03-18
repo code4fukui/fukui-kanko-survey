64dfaa2e8da20ad3bcaa34df1f6436dd89839348
Taisuke Fukuno <fukuno@jig.jp>
2024-03-18 10:20:34 +0900

    add all-cnt.csv

--- makeMonthly.js
+++ makeMonthly.js
@@ -4,21 +4,29 @@ import { dir2array } from "https://js.sabae.cc/dir2array.js";
 const list = (await dir2array("./daily")).filter(f => f.length == "20220527.csv".length && f.endsWith(".csv")).sort();
 const mlist = [];
 let mname = null;
+const cnts = [];
 const write = async () => {
   if (mname) {
     await Deno.writeTextFile("monthly/" + mname, CSV.stringify(mlist));
-    console.log(mname);
+    console.log("save", mname, mlist.length);
+    mlist.length = 0;
   }
 };
 for (const fn of list) {
   const csv = await CSV.fetchJSON("daily/" + fn);
+  const day = fn.substring(0, 8);
+  const ym = fn.substring(0, 6);
+
+  cnts.push({ day, count: csv.length });
+
   // monthly
-  const fn2 = fn.substring(0, 6) + ".csv";
+  const fn2 = ym + ".csv";
   if (fn2 != mname) {
-    write();
-    mlist.length = 0;
+    await write();
+    mname = fn2;
   }
-  mname = fn2;
   csv.forEach(c => mlist.push(c));
 }
-write();
+await write();
+
+await Deno.writeTextFile("./all-cnt.csv", CSV.stringify(cnts));
