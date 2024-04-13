1bc85c3c5498adc253a9cb74dedeceb057bf49df
Taisuke Fukuno <fukuno@jig.jp>
2024-04-13 09:08:18 +0900

    add makeByID.js

---
+++ makeByID.js
@@ -0,0 +1,20 @@
+import { CSV } from "https://js.sabae.cc/CSV.js";
+import { dir2array } from "https://js.sabae.cc/dir2array.js";
+
+await Deno.remove("byid", { recursive: true });
+await Deno.mkdir("byid", { recursive: true });
+
+const list = (await dir2array("./daily")).filter(f => f.length == "20220527.csv".length && f.endsWith(".csv")).sort();
+for (const fn of list) {
+  console.log(fn);
+  const csv = await CSV.fetchJSON("daily/" + fn);
+  for (const d of csv) {
+    const id = parseInt(d.会員ID);
+    const path = "byid/" + ((id / 1000) >> 0);
+    const fn = path + "/" + id + ".csv";
+    const data = await CSV.fetchJSON(fn, []);
+    data.push(d);
+    await Deno.mkdir(path, { recursive: true });
+    await Deno.writeTextFile(fn, CSV.stringify(data));
+  }
+}
